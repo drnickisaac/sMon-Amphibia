8cfdbfe016b57259d05c4fc1640975359efd98e3
Nick Isaac <isaacn@Seirians-MBP.local>
2018-01-23 18:08:26 +0100

    added trend calculation

--- Explore-Occupancy-output.R
+++ Explore-Occupancy-output.R
@@ -8,6 +8,24 @@ models <- lapply(sp_mods, function(sp) {
   return(out)
   }
   )
+names(models) <- gsub(sp_mods, pa="\\.rdata", repl="")
 
 # plot the occupancy
 lapply(models, plot)
+
+# now calculate the linear trends
+trends <- lapply(models, occurrenceChange, firstYear=1980, lastYear=2013)
+names(trends) <- gsub(sp_mods, pa="\\.rdata", repl="")
+
+outputs <- data.frame(
+  mean.trend = sapply(trends, function(x) x$mean),
+  CI.lower = sapply(trends, function(x) x$CIs[1]),
+  CI.upper = sapply(trends, function(x) x$CIs[2])) 
+
+# do we get a different answer starting in 2010?
+#sapply(models, occurrenceChange, firstYear=1980, lastYear=2010)
+
+# let's look at the midwife toad nin more detail
+summary(models$`Alytes obstetricans`)
+
+models$`Alytes obstetricans`$BUGSoutput$summary
