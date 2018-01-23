a75e623fe59a03292a3249a679a8e40e9df7dc37
Nick Isaac <isaacn@Seirians-MBP.local>
2018-01-23 16:50:53 +0100

    New file to explore and plot outputs

---
+++ Explore-Occupancy-output.R
@@ -0,0 +1,13 @@
+# explore model output
+
+# read in the model outputs
+list.files('model-outputs') -> sp_mods
+
+models <- lapply(sp_mods, function(sp) {
+  load(file=paste0('model-outputs/',sp))
+  return(out)
+  }
+  )
+
+# plot the occupancy
+lapply(models, plot)
