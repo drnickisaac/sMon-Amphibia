30b81f4b8f4dadc21d54189b393fca5721868ab0
Nick Isaac <isaacn@Seirians-MBP.local>
2018-01-23 18:11:56 +0100

    added new trend estimation

--- 02-run-simple-occmod.R
+++ 02-run-simple-occmod.R
@@ -2,7 +2,7 @@ library(sparta)
 
 # DAVID to modify to that this script now imports the clean version of the anura data
 
-##### let's set up a data set suitable for running an Occupancy model
+load('derived-data/anura_date_precision.rData')
 
 anura_fod <- formatOccData(taxa= as.character(anura$Species), 
                            site= as.character(anura$MTB_Q), 
@@ -17,7 +17,8 @@ table(anura_fod$occDetdata$L)
 # set the sparta options
 sparta_options <- c('ranwalk', # prior on occupancy is set by last year's posterior
                     'jul_date', # use the Julian date as a covariate on the detection probability
-                    'catlistlength') # categorises the visits into three sets of 'qualities'
+                    'catlistlength', # categorises the visits into three sets of 'qualities'
+                    'halfcauchy') # prior on the precisions
 
 #####  let's run a model. # this is really slow!
 anura_occmod <- occDetModel(taxa= as.character(anura$Species), 
