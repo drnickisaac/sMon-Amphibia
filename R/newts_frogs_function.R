8e645d0e8d4f13fb2b03a49b47d521dee0a319e0
Eichenberg <de25geka@idiv.de>
2018-01-23 11:52:48 +0100

    removed obsolete lines in the function newts_frogs

--- R/newts_frogs_function.R
+++ R/newts_frogs_function.R
@@ -1,8 +1,9 @@
 ## Function to subset news and frogs
+## the function takes the dataframe entered via the argument "data"
+## and adds a new colum called "Group" that assorts the amphibian species given in the column specified
+## by "taxon" to two groups, either "newts" or "frogs"
 newt_frog<- function (data=data, taxon=taxon){
   data$Group[data$taxon %in% c("Salamandra atra","Ichthyosaura alpestris","Lissotriton helveticus","Salamandra salamandra","Triturus cristatus","Lissotriton vulgaris")]<-c("newts")
   data$Group[!(data$taxon %in% c("Salamandra atra","Ichthyosaura alpestris","Lissotriton helveticus","Salamandra salamandra","Triturus cristatus","Lissotriton vulgaris"))]<-c("frogs")
   
 }
-newt_frog(amph_more_than_1_visit, taxon=Species)
-table(amph_more_than_1_visit$Group)
